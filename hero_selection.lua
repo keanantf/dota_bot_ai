7fd99b826435be05b6aab98b72fa9b0dcfd4cee2
Ben Somers <somers.ben@gmail.com>
2016-12-14 14:38:10 -0800

    add throttling & alternation to hero picks
    also switch algo to random - can switch back later

--- hero_selection.lua
+++ hero_selection.lua
@@ -1,161 +1,204 @@
+-- Copyright (c) 2016 somanyrobots (github.com/somanyrobots)
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+-- and associated documentation files (the "Software"), to deal in the Software without restriction,
+-- including without limitation the rights to use, copy, modify, merge, publish, distribute,
+-- sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in all copies or
+-- substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+-- BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+-- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+-- DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
 -- mandate that the bots will pick these heroes - for testing purposes
--- similarly grouped by chinese number system
-forcedHeroesByPosition = {
-  {},
-  {},
-  {},
-  {},
-  {}
+requiredHeroes = {
 };
 
--- grouped according to the chinese number system
-botHeroesByPosition = {
-	{
+-- change quickMode to true for testing
+-- quickMode eliminates the 30s delay before picks begin
+-- it also eliminates the delay between bot picks
+quickMode = false;
+
+allBotHeroes = {
+		'npc_dota_hero_axe',
+		'npc_dota_hero_bane',
 		'npc_dota_hero_bloodseeker',
+		'npc_dota_hero_bounty_hunter',
 		'npc_dota_hero_bristleback',
 		'npc_dota_hero_chaos_knight',
-		'npc_dota_hero_drow_ranger',
-		'npc_dota_hero_juggernaut',
-		'npc_dota_hero_luna',
-		'npc_dota_hero_necrolyte',
-		'npc_dota_hero_phantom_assassin',
-		'npc_dota_hero_sniper',
-		'npc_dota_hero_sven',
-		'npc_dota_hero_tiny',
-		'npc_dota_hero_skeleton_king'
-	},
-	{
-		'npc_dota_hero_axe',
-		'npc_dota_hero_bloodseeker',
-		'npc_dota_hero_bristleback',
+		'npc_dota_hero_crystal_maiden',
+		'npc_dota_hero_dazzle',
 		'npc_dota_hero_death_prophet',
 		'npc_dota_hero_dragon_knight',
 		'npc_dota_hero_drow_ranger',
+		'npc_dota_hero_earthshaker',
+		'npc_dota_hero_jakiro',
 		'npc_dota_hero_juggernaut',
 		'npc_dota_hero_kunkka',
-		'npc_dota_hero_lina',
-		'npc_dota_hero_luna',
-		'npc_dota_hero_necrolyte',
-		'npc_dota_hero_phantom_assassin',
-		'npc_dota_hero_pudge',
-		'npc_dota_hero_razor',
-		'npc_dota_hero_nevermore',
-		'npc_dota_hero_sniper',
-		'npc_dota_hero_tiny',
-		'npc_dota_hero_viper',
-		'npc_dota_hero_windrunner',
-		'npc_dota_hero_zuus'
-	},
-	{
-		'npc_dota_hero_axe',
-		'npc_dota_hero_bounty_hunter',
-		'npc_dota_hero_bristleback',
-		'npc_dota_hero_dragon_knight',
-		'npc_dota_hero_earthshaker',
-		'npc_dota_hero_omniknight',
-		'npc_dota_hero_pudge',
-		'npc_dota_hero_sand_king',
-		'npc_dota_hero_tidehunter',
-		'npc_dota_hero_windrunner'
-	},
-	{
-		'npc_dota_hero_axe',
-		'npc_dota_hero_bane',
-		'npc_dota_hero_bounty_hunter',
-		'npc_dota_hero_crystal_maiden',
-		'npc_dota_hero_dazzle',
-		'npc_dota_hero_earthshaker',
-		'npc_dota_hero_jakiro',
-		'npc_dota_hero_kunkka',
-		'npc_dota_hero_lina',
-		'npc_dota_hero_lion',
-		'npc_dota_hero_omniknight',
-		'npc_dota_hero_pudge',
-		'npc_dota_hero_sand_king',
-		'npc_dota_hero_skywrath_mage',
-		'npc_dota_hero_tidehunter',
-		'npc_dota_hero_vengefulspirit',
-		'npc_dota_hero_warlock',
-		'npc_dota_hero_windrunner',
-		'npc_dota_hero_witch_doctor'
-	},
-	{
-		'npc_dota_hero_bane',
-		'npc_dota_hero_bounty_hunter',
-		'npc_dota_hero_crystal_maiden',
-		'npc_dota_hero_dazzle',
-		'npc_dota_hero_earthshaker',
-		'npc_dota_hero_jakiro',
 		'npc_dota_hero_lich',
 		'npc_dota_hero_lina',
 		'npc_dota_hero_lion',
+		'npc_dota_hero_luna',
+		'npc_dota_hero_necrolyte',
+		'npc_dota_hero_nevermore',
 		'npc_dota_hero_omniknight',
 		'npc_dota_hero_oracle',
+		'npc_dota_hero_phantom_assassin',
+		'npc_dota_hero_pudge',
+		'npc_dota_hero_razor',
+		'npc_dota_hero_sand_king',
+		'npc_dota_hero_skeleton_king',
 		'npc_dota_hero_skywrath_mage',
+		'npc_dota_hero_sniper',
+		'npc_dota_hero_sven',
+		'npc_dota_hero_tidehunter',
+		'npc_dota_hero_tiny',
 		'npc_dota_hero_vengefulspirit',
+		'npc_dota_hero_viper',
 		'npc_dota_hero_warlock',
-		'npc_dota_hero_witch_doctor'
-	}
+		'npc_dota_hero_windrunner',
+		'npc_dota_hero_witch_doctor',
+		'npc_dota_hero_zuus'
 };
+
+picks = {};
 
 -- TODO
 -- 1. determine which slots contain players - don't pick for those slots
--- 2. determine what heroes players have picked - don't pick those heroes - DONE
--- 3. implement a delay so we don't pick all the heroes at once
+-- 2. determine what heroes have already been picked - don't pick those heroes - DONE
 -- 4. copy the default behavior of picking everything at once after players have picked, though
--- 5. alternate picks between teams, so the selection is a little fairer
+-- 5. add some jitter, so the bots pick at slightly more random times
+-- 6. reimplement farm priority based system
 function Think()
 	local radiantOffset = -1;
 	local direOffset = 4;
+  local startPickTime = -45;
+  local timePerPick = 5;
 
-	if ( GetTeam() == TEAM_RADIANT and radiantTeamSelected ~= true ) then
-		print("picking radiant team");
-		radiantTeamSelected = DraftTeam( radiantOffset );
-	elseif ( GetTeam() == TEAM_DIRE and direTeamSelected ~= true ) then
-		print("picking dire team");
-		direTeamSelected = DraftTeam( direOffset );
+  local radiantSlots = {0,1,2,3,4};
+  local direSlots = {5,6,7,8,9};
+
+  if not quickMode and (DotaTime() < startPickTime) then
+    return;
+  end
+
+  picks = GetPicks();
+  local pickCount = 0;
+  for k,v in pairs(picks) do -- have to iterate here, as conditions are not right to use #
+    pickCount = pickCount + 1;
+  end
+  local pickTime = startPickTime + (timePerPick * pickCount);
+
+  if not quickMode and  (DotaTime() < pickTime) then
+    return;
+  end
+
+	if ( GetTeam() == TEAM_RADIANT and IsTeamsTurnToPick(TEAM_RADIANT)) then
+		for i, potentialSlot in pairs(radiantSlots) do
+      print("evaluating slot ", potentialSlot);
+			if (IsSlotEmpty(potentialSlot)) then
+				PickHero(potentialSlot);
+  			return;
+			end
+		end
+	elseif ( GetTeam() == TEAM_DIRE and IsTeamsTurnToPick(TEAM_DIRE)) then
+		for i, potentialSlot in pairs(direSlots) do
+			if (IsSlotEmpty(potentialSlot)) then
+				PickHero(potentialSlot);
+  			return;
+			end
+		end
 	end
-
 end
 
--- takes an offset value to compare chinese positions w/ hero slots
-function DraftTeam( offset )
-	for i=1, 5 do
-		local hero = GetHeroForPosition( i );
-		print("picked " .. hero .. " for slot " .. (i + offset));
-		SelectHero( i + offset, hero );
-	end
-	return true;
+function IsTeamsTurnToPick(team)
+  local radiantHeroCount = 0;
+  local direHeroCount = 0;
+  for pickedSlot, hero in pairs(picks) do
+    if slotBelongsToTeam(pickedSlot, TEAM_RADIANT) then
+      radiantHeroCount = radiantHeroCount + 1;
+    else
+      direHeroCount = direHeroCount + 1;
+    end
+  end
+  if (team == TEAM_RADIANT) then
+    return (radiantHeroCount <= direHeroCount);
+  else
+    return (direHeroCount <= radiantHeroCount);
+  end
+end
+
+function IsSlotEmpty(slot)
+  local slotEmpty = true;
+  for pickedSlot, hero in pairs(picks) do
+    print("pickedSlot is", pickedSlot);
+    print("hero is", hero);
+		if (pickedSlot == slot) then
+			slotEmpty = false;
+		end
+  end
+  print("slotempty is ", slotEmpty);
+  return slotEmpty;
+end
+
+function PickHero(slot)
+  local hero = GetRandomHero();
+  print("picking hero ", hero, " for slot ", slot);
+  SelectHero(slot, hero);
 end
 
 -- haven't found a better way to get already-picked heroes than just looping over all the players
-function GetPickedHeroes()
+function GetPicks()
 	local selectedHeroes = {};
+  local pickedSlots = {};
 	for i=0,9 do
 		local hName = GetSelectedHeroName(i);
-		if (hName ~= nil) then
-			selectedHeroes[hName] = true;
+		if (hName ~= nil and hName ~= "") then
+			selectedHeroes[i] = hName;
 		end
 	end
 	return selectedHeroes;
 end
 
+-- PLACEHOLDER
+-- need to figure out an actual way to determine this, not just hardcoding it
+function slotBelongsToTeam(slot, team)
+  if (team == TEAM_RADIANT) then
+    for i=0, 4 do
+      if (slot == i) then return true end;
+    end
+  elseif (team == TEAM_DIRE) then
+    for i=5, 9 do
+      if (slot == i) then return true end;
+    end
+  end
+	return false;
+end
 
-function GetHeroForPosition( position )
+-- first, check the list of required heroes and pick from those
+-- then try the whole bot pool
+function GetRandomHero()
 	local hero;
-	local forcedHeroOptions = forcedHeroesByPosition[position];
-	local positionHeroOptions = botHeroesByPosition[position];
-	local selectedHeroes = GetPickedHeroes();
+	local picks = GetPicks();
+  local selectedHeroes = {};
+  for slot, hero in pairs(picks) do
+    selectedHeroes[hero] = true;
+  end
 
-	hero = forcedHeroOptions[RandomInt(1, #forcedHeroOptions)];
+	hero = requiredHeroes[RandomInt(1, #requiredHeroes)];
 	if (hero == nil) then
-		hero = positionHeroOptions[RandomInt(1, #positionHeroOptions)];
+		hero = allBotHeroes[RandomInt(1, #allBotHeroes)];
 	end
-
 
 	while ( selectedHeroes[hero] == true ) do
 		print("repicking because " .. hero .. " was taken.")
-		hero = positionHeroOptions[RandomInt(1, #positionHeroOptions)];
+		hero = allBotHeroes[RandomInt(1, #allBotHeroes)];
 	end
 
 	return hero;
